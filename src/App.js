a59ad32ded3cc9c1d4e88ec766942f612206bd8d
Konstantin Halkin <constantingal@gmail.com>
2021-08-08 11:59:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import Container from "./components/Container/Container";
 import AppBar from "./components/AppBar/AppBar";
-import HomePage from "./components/HomePage/HomePage";
-import MoviesPage from "./components/MoviesPage/MoviesPage";
-import MovieDetailsPage from "./components/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+
+const HomePage = lazy(() => import("./components/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./components/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("./components/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() =>
+  import("./components/NotFoundPage/NotFoundPage")
+);
 
 function App() {
   return (
@@ -15,23 +21,25 @@ function App() {
           <AppBar />
         </header>
 
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
+        <Suspense fallback={<h1>Загружаем...</h1>}>
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
 
-          <Route path="/movies" exact>
-            <MoviesPage />
-          </Route>
+            <Route path="/movies" exact>
+              <MoviesPage />
+            </Route>
 
-          <Route path="/movies/:movieId">
-            <MovieDetailsPage />
-          </Route>
+            <Route path="/movies/:movieId">
+              <MovieDetailsPage />
+            </Route>
 
-          <Route>
-            <NotFoundPage />
-          </Route>
-        </Switch>
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Suspense>
       </div>
     </Container>
   );
